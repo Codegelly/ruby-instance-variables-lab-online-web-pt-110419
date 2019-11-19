f276bf69afa0e36386f7170c67950fddb93ff6ff
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-19 05:00:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,11 +7,11 @@ def name=(dog_name)
     this_dogs_name
   end
   
-   def lassie = Dog.new
- lassie.name = "Lassie"
+#   def lassie = Dog.new
+# lassie.name = "Lassie"
  
- lassie.name 
- end 
+# lassie.name 
+# end 
 end 
   
   
